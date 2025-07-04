6de2258a4ee6f2f49e97c5f4bc64a5caf2dc4f69
mr-mani921 <usman.mani.10011@gmail.com>
2025-07-04 10:54:51 +0500

    first task learned, improved

--- user-api/src/users/users.service.ts
+++ user-api/src/users/users.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users = [
+  private users: User[] = [
     { id: 1, name: 'mani', type: 'normal' },
     { id: 2, name: 'raza', type: 'normal' },
   ];
 
-  getAllUsers() {
+  getAllUsers(): User[] {
     return this.users;
   }
 
-  registerUser(user: CreateUserDTO) {
-    const id = Date.now();
+  private generateId(): number {
+    return this.users.length + 1;
+  }
+  
+
+  registerUser(user: CreateUserDTO): User[] {
+    const id = this.generateId();
     this.users.push({ id, ...user });
     return this.users;
   }
